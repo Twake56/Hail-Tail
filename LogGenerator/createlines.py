6446928675ca477f51e76e1288562ced2e7b0c90
Trevor Wakeman <twakeman@accurence.com>
2019-04-22 15:01:14 -0600

    Making Git ignore work

--- LogGenerator/createlines.py
+++
@@ -1,9 +0,0 @@
-import time
-
-num = 1
-while True:
-    with open('C:\\Users\\twakeman\\Documents\\GitHub\\Timber-Tail/LogGenerator/test.log', "a+") as log:
-        log.write('New logline' + str(num) + '\n')
-        num += 1
-        log.close()
-        time.sleep(1)
